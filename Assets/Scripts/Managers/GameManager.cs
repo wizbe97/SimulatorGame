5f5248b3ff0a9065bf876d41222503b8a7005154
wizbe97 <nathanblore97@gmail.com>
2025-08-25 19:06:05 +0100

    Setting up managers

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; }
+    [Header("Prefabs")]
+    [SerializeField] private GameObject[] menuManagerPrefabs;
+    [SerializeField] private GameObject[] gameplayManagerPrefabs;
+
+    [Header("Settings")]
+    [Header("Debug Variables")]
+    private bool isOnMenu;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Menu")
+            isOnMenu = true;
+        else
+            isOnMenu = false;
+
+        SpawnManagers(isOnMenu);
+    }
+
+    private void SpawnManagers(bool isOnMenu)
+    {
+        if (isOnMenu)
+            foreach (var prefab in menuManagerPrefabs)
+                Instantiate(prefab);
+        else
+            foreach (var prefab in gameplayManagerPrefabs)
+                Instantiate(prefab);
     }
 }
